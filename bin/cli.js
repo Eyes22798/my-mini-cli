f03f4e51a3fb6ad732d6fb2b759b6521cf78a5e3
eyes22798 <eyes22798@gmail.com>
2021-06-19 14:55:16 +0800

    init commit

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+const program = require('commander')
+
+program
+  .option('-c, --cheese <type>', 'add the specified type of cheese', 'blue');
+
+program.parse();
+
+console.log(`cheese: ${program.opts().cheese}`);
+console.log(options, process.argv)
